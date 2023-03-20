0b66bd801a83a4fed7027ba8d18dee541204901b
mhogervo <matthijs.hogervorst@gmail.com>
2023-03-20 16:18:29 +0100

    cleaned up

--- rand.cpp
+++ rand.cpp
@@ -6,18 +6,25 @@
 #include <map>
 #include <stdlib.h> 
 
-double gamma_moments(int, double, double);
-std::pair<double, double> stats_from_sample(const std::vector<double>&);
+// double gamma_moments(int, double, double);
 
 typedef std::map<std::string, double> parameterSet;
+typedef std::pair<double, double> doublePair;
+
 std::vector<double> simulate_GBM(parameterSet, int, std::mt19937_64&);
+doublePair stats_GBM(parameterSet);
+
 std::vector<double> simulate_VG(parameterSet, int, std::mt19937_64&);
+doublePair stats_VG(parameterSet);
+
+doublePair stats_from_sample(const std::vector<double>&);
+void print_stats(const doublePair, const doublePair);
 
 int main()
 {
-    
     int my_seed = 2023;
-    int num_samples = 1e6;
+    int num_samples = 2e4;
+    int n_steps = 150; // steps for every single walk; total calls to the RNG is num_samples * n_steps.
 
     std::seed_seq seed_seq{my_seed};
     std::mt19937_64 rng(seed_seq);
@@ -25,54 +32,37 @@ int main()
     // std::ranlux24 rng(seed_seq);
     // Mersenne is faster and still acceptable. The _64 version is slightly better.
     // std::mt19937 rng(seed_seq);
-    
-    // I: Simulate num_samples different geometric Brownian motions with specified parameters.
 
-    num_samples = 1e5;
+    // I: Simulate num_samples different geometric Brownian motions with specified parameters.
+   
     parameterSet brownianParams = {{"sigma", 0.3}, {"mu", 1}, {"S0", 100}, {"T", 0.7}};
-    int n_steps = 100; // steps for every single walk; total calls to the RNG is num_samples * n_steps.
-    
     std::vector<double> sample_list = {};
     for (int i=0; i<num_samples; i++) {
         auto sim = simulate_GBM(brownianParams, n_steps, rng);
         sample_list.push_back(sim.back());
     }
     // extract statistics and compare to analytics:
-    auto sample_stats = stats_from_sample(sample_list);
-    double mu_est = sample_stats.first, sigma_est = sample_stats.second;
-    
-    double S0 = brownianParams["S0"], T = brownianParams["T"];
-    double mu = brownianParams["mu"], sigma = brownianParams["sigma"];
-    double exp_S_T = S0*exp(mu*T);
-    double std_S_T = exp_S_T * sqrt( exp(pow(sigma,2)*T) - 1 );
+    doublePair sample_stats = stats_from_sample(sample_list);
+    doublePair stats_th = stats_GBM(brownianParams);
     
     std::cout << "\nComparing sample mean and standard deviation (GBM), over " << num_samples << " realizations:" << std::endl;
-    std::cout << exp_S_T << "\t" << mu_est << "\n" << std_S_T << "\t" << sigma_est << "\n" << std::endl;
+    print_stats(stats_th, sample_stats);
+
+    // -------------------------------------------
     
     // II: Simulate num_samples different VG processes with specified parameters.
-
-    num_samples = 1e5;
-    parameterSet VGParams = {{"theta", 3.2}, {"sigma", 2.4}, {"nu", 0.7}, {"S0", 30.}, {"T", 0.4}};
-    n_steps = 100; // steps for every single walk; total calls to the RNG is num_samples * n_steps.
     
+    parameterSet VGParams = {{"theta", -1.2}, {"sigma", 2.4}, {"nu", 0.7}, {"S0", 30.}, {"T", 0.4}};
     sample_list = {};
     for (int i=0; i<num_samples; i++) {
         auto sim = simulate_VG(VGParams, n_steps, rng);
         sample_list.push_back(sim.back());
     }
-    // extract statistics and compare to analytics:
     sample_stats = stats_from_sample(sample_list);
-    mu_est = sample_stats.first, sigma_est = sample_stats.second;
-    
-    
-    double theta = VGParams["theta"], nu = VGParams["nu"];
-    sigma = VGParams["sigma"], S0 = VGParams["S0"], T = VGParams["T"];
-    double exp_VG = S0 + theta*T;
-    double std_VG = sqrt((pow(theta,2)*nu + pow(sigma,2))*T);
+    stats_th = stats_VG(VGParams);
     
     std::cout << "Comparing sample mean and standard deviation (VG), over " << num_samples << " realizations:" << std::endl;
-    std::cout << exp_VG << "\t" << mu_est << "\n" << std_VG << "\t" << sigma_est << "\n" << std::endl;
-    
+    print_stats(stats_th, sample_stats);
     
 }
 
@@ -80,7 +70,7 @@ int main()
 
 
 
-std::pair<double, double> stats_from_sample(const std::vector<double> & v) {
+doublePair stats_from_sample(const std::vector<double> & v) {
     /**
        Given a vector v of floats, compute the mean and standard deviation.
        
@@ -109,19 +99,27 @@ std::pair<double, double> stats_from_sample(const std::vector<double> & v) {
 }
 
 
-double gamma_moments(int n, double alpha=1.0, double beta=1.0) {
-    /**
-       Return the n-th moment of the gamma distribution:
-       
-           (alpha)_n * beta^n
-       
-       where (x)_n = x(x+1)...(x+n-1) is the Pochhammer symbol.
+
+doublePair stats_GBM(parameterSet params) {
+    /* Given a set of parameters for the GBM, compute the expected mean and standard deviation,
+       returned as a pair.
     */
-    
-    double out = 1;
-    for (int i=0; i<n; i++) out *= alpha + i;
-    out *= pow(beta,n);
-    return out;
+    double mu, sigma, S0, T;
+    if (params.contains("mu") and params.contains("sigma") and
+        params.contains("S0") and params.contains("T")) {
+        mu = params["mu"], sigma = params["sigma"];
+        S0 = params["S0"], T = params["T"];
+    }
+    else {
+        std::cerr << "Error: parameter set for Brownian motion is not complete." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    double mean = S0*exp(mu*T);
+    double std = mean * sqrt( exp(pow(sigma,2)*T) - 1 );
+
+    doublePair out(mean, std);
+    return out;    
 }
 
 std::vector<double> simulate_GBM(parameterSet params, int N, std::mt19937_64& rng) {
@@ -157,6 +155,27 @@ std::vector<double> simulate_GBM(parameterSet params, int N, std::mt19937_64& rn
     return samples;
 }
 
+doublePair stats_VG(parameterSet params) {
+    /* Given a set of parameters for the VG process, compute the expected mean and standard deviation,
+       returned as a pair.
+    */
+    double theta, sigma, nu, S0, T;
+    if (params.contains("theta") and params.contains("sigma") and params.contains("nu")
+        and params.contains("S0") and params.contains("T")) {
+        theta = params["theta"], sigma = params["sigma"], nu = params["nu"];
+        S0 = params["S0"], T = params["T"];
+    }
+    else {
+        std::cerr << "Error: parameter set for the variance-gamma process is not complete." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    double mean = S0 + theta*T;
+    double std = sqrt((pow(theta,2)*nu + pow(sigma,2))*T);
+    
+    doublePair out(mean, std);
+    return out;    
+}
 
 std::vector<double> simulate_VG(parameterSet params, int N, std::mt19937_64& rng) {
     /** Simulate a variance-gamma process with parameters {theta, sigma, nu}.
@@ -193,3 +212,25 @@ std::vector<double> simulate_VG(parameterSet params, int N, std::mt19937_64& rng
     return samples;
 }
 
+void print_stats(const doublePair stats_th, const doublePair stats_sample) {
+    /* Given two pairs of (mu, sigma) in theory and for a sample, print the comparison. */
+    double mu_est = stats_sample.first, sigma_est = stats_sample.second;
+    double mu_th = stats_th.first, sigma_th = stats_th.second;
+    std::cout << mu_th << "\t" << mu_est << "\n" << sigma_th << "\t" << sigma_est << "\n" << std::endl;
+}
+
+
+double gamma_moments(int n, double alpha=1.0, double beta=1.0) {
+    /**
+       Return the n-th moment of the gamma distribution:
+       
+           (alpha)_n * beta^n
+       
+       where (x)_n = x(x+1)...(x+n-1) is the Pochhammer symbol.
+    */
+    
+    double out = 1;
+    for (int i=0; i<n; i++) out *= alpha + i;
+    out *= pow(beta,n);
+    return out;
+}
