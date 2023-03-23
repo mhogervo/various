9b27c913edfe58ee573dabd4d4cee26c3d3184eb
mhogervo <matthijs.hogervorst@gmail.com>
2023-03-23 09:32:34 +0100

    better practices

--- rand.cpp
+++ rand.cpp
@@ -4,16 +4,19 @@
 #include <math.h>
 #include <string>
 #include <map>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <set>
 
 typedef std::map<std::string, double> parameterSet;
 typedef std::pair<double, double> doublePair;
 
-std::vector<double> simulate_GBM(parameterSet, int, std::mt19937_64&);
-doublePair stats_GBM(parameterSet);
+bool check_GBM_params(const parameterSet&);
+std::vector<double> simulate_GBM(const parameterSet&, int, std::mt19937_64&);
+doublePair stats_GBM(const parameterSet&);
 
-std::vector<double> simulate_VG(parameterSet, int, std::mt19937_64&);
-doublePair stats_VG(parameterSet);
+bool check_VG_params(const parameterSet&);
+std::vector<double> simulate_VG(const parameterSet&, int, std::mt19937_64&);
+doublePair stats_VG(const parameterSet&);
 
 doublePair stats_from_sample(const std::vector<double>&);
 void print_stats(const doublePair, const doublePair);
@@ -71,9 +74,6 @@ int main()
 }
 
 
-
-
-
 doublePair stats_from_sample(const std::vector<double> & v) {
     /**
        Given a vector v of floats, compute the mean and standard deviation.
@@ -102,31 +102,38 @@ doublePair stats_from_sample(const std::vector<double> & v) {
     return out;
 }
 
+bool check_GBM_params(const parameterSet& p) {
+    /* Take a parameterSet and check if it contains all parameter for the GBM process. */
+    std::set<std::string> tokens {"mu", "sigma", "S0", "T"};
+    for (const auto& s : tokens) {
+        if (p.count(s) == 0) return false;
+    }
+    return true;
+}
 
-
-doublePair stats_GBM(parameterSet params) {
+doublePair stats_GBM(const parameterSet& params) {
     /* Given a set of parameters for the GBM, compute the expected mean and standard deviation,
        returned as a pair.
     */
     double mu, sigma, S0, T;
-    if (params.contains("mu") and params.contains("sigma") and
-        params.contains("S0") and params.contains("T")) {
-        mu = params["mu"], sigma = params["sigma"];
-        S0 = params["S0"], T = params["T"];
+    if (check_GBM_params(params)) {
+        sigma = params.at("sigma");
+        mu = params.at("mu");
+        S0 = params.at("S0");
+        T = params.at("T");
     }
     else {
-        std::cerr << "Error: parameter set for Brownian motion is not complete." << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Error: parameter set for the GBM process is not complete.");
     }
 
     double mean = S0*exp(mu*T);
-    double std = mean * sqrt( exp(pow(sigma,2)*T) - 1 );
+    double std_dev = mean * sqrt( exp(pow(sigma,2)*T) - 1 );
 
-    doublePair out(mean, std);
+    auto out = std::make_pair(mean, std_dev);
     return out;    
 }
 
-std::vector<double> simulate_GBM(parameterSet params, int N, std::mt19937_64& rng) {
+std::vector<double> simulate_GBM(const parameterSet& params, int N, std::mt19937_64& rng) {
     /** Simulate a geometric Brownian motion. The map params should contain {mu, sigma, T, S0}.
         N is the number of steps.
 
@@ -135,15 +142,15 @@ std::vector<double> simulate_GBM(parameterSet params, int N, std::mt19937_64& rn
     
     std::vector<double> samples(N+1);
     double mu, sigma, S0, T, dt;
-    if (params.contains("mu") and params.contains("sigma") and
-        params.contains("S0") and params.contains("T")) {
-        mu = params["mu"], sigma = params["sigma"];
-        S0 = params["S0"], T = params["T"];
+    if (check_GBM_params(params)) {
+        sigma = params.at("sigma");
+        mu = params.at("mu");
+        S0 = params.at("S0");
+        T = params.at("T");
         dt = T/N;
     }
     else {
-        std::cerr << "Error: parameter set for Brownian motion is not complete." << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Error: parameter set for the GBM process is not complete.");
     }
     
     double mu_bar = (mu - 0.5*pow(sigma,2))*dt, sigma_bar = sigma*sqrt(dt);
@@ -159,29 +166,40 @@ std::vector<double> simulate_GBM(parameterSet params, int N, std::mt19937_64& rn
     return samples;
 }
 
-doublePair stats_VG(parameterSet params) {
+bool check_VG_params(const parameterSet& p) {
+    /* Take a parameterSet and check if it contains all parameter for the VG process. */
+    std::set<std::string> tokens {"theta", "sigma", "nu", "S0", "T"};
+    for (const auto& s : tokens) {
+        if (p.count(s) == 0) return false;
+    }
+    return true;
+}
+
+doublePair stats_VG(const parameterSet& params) {
     /* Given a set of parameters for the VG process, compute the expected mean and standard deviation,
        returned as a pair.
     */
     double theta, sigma, nu, S0, T;
-    if (params.contains("theta") and params.contains("sigma") and params.contains("nu")
-        and params.contains("S0") and params.contains("T")) {
-        theta = params["theta"], sigma = params["sigma"], nu = params["nu"];
-        S0 = params["S0"], T = params["T"];
+    if (check_VG_params(params)) {
+        theta = params.at("theta");
+        sigma = params.at("sigma");
+        nu = params.at("nu");
+        S0 = params.at("S0");
+        T = params.at("T");
     }
     else {
-        std::cerr << "Error: parameter set for the variance-gamma process is not complete." << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Error: parameter set for the variance-gamma process is not complete.");
     }
 
     double mean = S0 + theta*T;
-    double std = sqrt((pow(theta,2)*nu + pow(sigma,2))*T);
+    double std_dev = sqrt((pow(theta,2)*nu + pow(sigma,2))*T);
     
-    doublePair out(mean, std);
+    auto out = std::make_pair(mean, std_dev);
     return out;    
 }
 
-std::vector<double> simulate_VG(parameterSet params, int N, std::mt19937_64& rng) {
+
+std::vector<double> simulate_VG(const parameterSet& params, int N, std::mt19937_64& rng) {
     /** Simulate a variance-gamma process with parameters {theta, sigma, nu}.
         The map params should also contain S0 and the total time T.
         N is the number of steps, such that dt = T/N.
@@ -191,15 +209,16 @@ std::vector<double> simulate_VG(parameterSet params, int N, std::mt19937_64& rng
     
     std::vector<double> samples(N+1);
     double theta, sigma, nu, S0, T, dt;
-    if (params.contains("theta") and params.contains("sigma") and params.contains("nu")
-        and params.contains("S0") and params.contains("T")) {
-        theta = params["theta"], sigma = params["sigma"], nu = params["nu"];
-        S0 = params["S0"], T = params["T"];
+    if (check_VG_params(params)) {
+        theta = params.at("theta");
+        sigma = params.at("sigma");
+        nu = params.at("nu");
+        S0 = params.at("S0");
+        T = params.at("T");
         dt = T/N;
     }
     else {
-        std::cerr << "Error: parameter set for the variance-gamma process is not complete." << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Error: parameter set for the variance-gamma process is not complete.");
     }
     
     std::gamma_distribution gamma_distr(dt/nu, nu);
